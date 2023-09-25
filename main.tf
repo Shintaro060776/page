c357ba914aa160c780acb3b3e238a7b97150d45d
shin20090317 <shintaro060776@gmail.com>
2023-09-25 20:39:49 +0900

    1

--- main.tf
+++ main.tf
@@ -1,95 +1,14 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "main-vpc1"
+terraform {
+  required_version = ">= 0.12"
+
+  backend "s3" {
+    bucket = "vhrthrtyergtcere"
+    key    = "terraform.tfstate"
+    region = "ap-northeast-1"
   }
 }
 
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  tags = {
-    Name = "main-subnet"
-  }
-}
 
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-}
-
-resource "aws_route_table" "main" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-}
-
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.main.id
-  route_table_id = aws_route_table.main.id
-}
-
-resource "aws_security_group" "ec2" {
-  vpc_id = aws_vpc.main.id
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = var.DEPLOY_PUBLIC_KEY
-}
-
-resource "aws_eip" "example" {
-  instance = aws_instance.web.id
-  domain   = "vpc"
-}
-
-resource "aws_instance" "web" {
-  ami                    = "ami-03a1b4db103179555"
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.main.id
-  vpc_security_group_ids = [aws_security_group.ec2.id]
-  key_name               = aws_key_pair.deployer.key_name
-
-  tags = {
-    Name = "WebServer"
-  }
-
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo yum update -y
-    sudo amazon-linux-extras install nginx1
-    sudo systemctl start nginx
-    sudo systemctl enable nginx
-    echo "Hello from nginx" > /usr/share/nginx/html/index.html
-    EOF
+provider "aws" {
+  region = "ap-northeast-1"
 }
--- variables.tf
+++
@@ -1,4 +0,0 @@
-variable "DEPLOY_PUBLIC_KEY" {
-  description = "The SSH public key for deployment"
-  default     = ""
-}
