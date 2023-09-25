b33d8344d069e21f0e842806feec088b39361d1c
shin20090317 <shintaro060776@gmail.com>
2023-09-25 17:07:32 +0900

    Formatted Terraform files

--- main.tf
+++ main.tf
@@ -1,71 +1,71 @@
 resource "aws_vpc" "main" {
-    cidr_block = "10.0.0.0/16"
-    tags = {
-        Name = "main-vpc"
-    }
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main-vpc"
+  }
 }
 
 resource "aws_subnet" "main" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.1.0/24"
-    tags = {
-        Name = "main-subnet"
-    }
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "main-subnet"
+  }
 }
 
 resource "aws_internet_gateway" "main" {
-    vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 }
 
 resource "aws_route_table" "main" {
-    vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.main.id
-    }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
 }
 
 resource "aws_route_table_association" "a" {
-    subnet_id = aws_subnet.main.id
-    route_table_id = aws_route_table.main.id
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
 }
 
 resource "aws_security_group" "ec2" {
-    vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    ingress {
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_key_pair" "deployer" {
-    key_name = "deployer-key"
-    public_key = file("~/.ssh/id_rsa.pub")
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
 resource "aws_eip" "example" {
@@ -74,17 +74,17 @@ resource "aws_eip" "example" {
 }
 
 resource "aws_instance" "web" {
-    ami = "ami-03a1b4db103179555"
-    instance_type = "t2.micro"
-    subnet_id = aws_subnet.main.id
-    vpc_security_group_ids = [aws_security_group.ec2.id]
-    key_name = aws_key_pair.deployer.key_name
+  ami                    = "ami-03a1b4db103179555"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.ec2.id]
+  key_name               = aws_key_pair.deployer.key_name
 
-    tags = {
-        Name = "WebServer"
-    }
+  tags = {
+    Name = "WebServer"
+  }
 
-      user_data = <<-EOF
+  user_data = <<-EOF
     #!/bin/bash
     sudo yum update -y
     sudo amazon-linux-extras install nginx1
