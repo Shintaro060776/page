13f346ec27edc337b2afd7c392ef2829961d05c2
shin20090317 <shintaro060776@gmail.com>
2023-09-25 17:43:43 +0900

    7

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ resource "aws_security_group" "ec2" {
 
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
-  public_key = file("~/.ssh/id_rsa.pub")
+  public_key = var.ssh_public_key
 }
 
 resource "aws_eip" "example" {
