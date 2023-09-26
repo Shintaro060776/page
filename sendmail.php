3934b5f0e38c3b3929bc3b492b2de782779396eb
shin20090317 <shintaro060776@gmail.com>
2023-09-26 19:04:05 +0900

    9

--- sendmail.php
+++ sendmail.php
@@ -1,45 +1,57 @@
 <?php
+
 require 'vendor/autoload.php';
 
 use Aws\Ses\SesClient;
+use Aws\Exception\AwsException;
 
 $credentials = require 'credentials.php';
+
+$SesClient = new SesClient([
+    'version' => 'latest',
+    'region'  => 'ap-northeast-1',
+    'credentials' => $credentials
+]);
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $name = isset($_POST['name']) ? $_POST['name'] : '';
     $email = isset($_POST['email']) ? $_POST['email'] : '';
     $content = isset($_POST['content']) ? $_POST['content'] : '';
 
-    $client = SesClient::factory(array(
-        'version'=> 'latest',
-        'region' => 'ap-northeast-1',
-        'credentials' => $credentials
-    ));
+    $subject = "お問い合わせ from " . $name;
+    $plaintext_body = $content;
+    $html_body = "<p>{$content}</p>";
+    $char_set = 'UTF-8';
 
-    $result = $client->sendEmail([
-        'Destination' => [
-            'ToAddresses' => ['shintaro060776@gmail.com'],
-        ],
-        'Message' => [
-            'Body' => [
-                'Text' => [
-                    'Data' => $content,
-                    'Charset' => 'UTF-8',
+    try {
+        $result = $SesClient->sendEmail([
+            'Destination' => [
+                'ToAddresses' => ['shintaro060776@gmail.com'],
+            ],
+            'ReplyToAddresses' => [$email],
+            'Source' => $email,
+            'Message' => [
+                'Body' => [
+                    'Html' => [
+                        'Charset' => $char_set,
+                        'Data' => $html_body,
+                    ],
+                    'Text' => [
+                        'Charset' => $char_set,
+                        'Data' => $plaintext_body,
+                    ],
+                ],
+                'Subject' => [
+                    'Charset' => $char_set,
+                    'Data' => $subject,
                 ],
             ],
-            'Subject' => [
-                'Data' => "お問い合わせ from " . $name,
-                'Charset' => 'UTF-8',
-            ],
-        ],
-        'Source' => $email,
-    ]);
+        ]);
 
-    if (!$result) {
-        echo "メールの送信に失敗しました。";
-        exit; 
+        header('Location: ./thankyou/thankyou.html');
+    } catch (AwsException $e) {
+        echo $e->getMessage();
+        echo("The email was not sent. Error message: ".$e->getAwsErrorMessage()."\n");
     }
-
-    header('Location: ./thankyou/thankyou.html');
 }
 ?>
