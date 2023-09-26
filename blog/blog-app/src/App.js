9f43dd576646cf18912cd3fd819ae11ffbecc883
shin20090317 <shintaro060776@gmail.com>
2023-09-27 03:38:04 +0900

    35

---
+++ blog/blog-app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>My Blog</h1>
+      </header>
+      <main>
+        <h2>Blog Post Title</h2>
+        <p>This is the content of the blog post.</p>
+        {/* 他のブログポストもここに追加できます */}
+      </main>
+    </div>
+  );
+}
+
+export default App;
