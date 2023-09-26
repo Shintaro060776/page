0fbf05e590448e8477141e2fd9efaaf319d743c0
shin20090317 <shintaro060776@gmail.com>
2023-09-26 19:52:40 +0900

    3

--- sendmail.php
+++ sendmail.php
@@ -35,7 +35,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
                 'ToAddresses' => ['shintaro060776@gmail.com'],
             ],
             'ReplyToAddresses' => [$email],
-            'Source' => $email,
+            'Source' => 'default_sender@example.com',
             'Message' => [
                 'Body' => [
                     'Html' => [
