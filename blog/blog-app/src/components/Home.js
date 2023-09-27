67db5fd41ad542d3324d7d8034df66dc030abf29
shin20090317 <shintaro060776@gmail.com>
2023-09-27 18:52:49 +0900

    56

--- blog/blog-app/src/components/Home.js
+++ blog/blog-app/src/components/Home.js
@@ -4,13 +4,13 @@ function Home() {
 
     const posts = [
         { id: 1, title: "First post", content: "This is the first post." },
-
     ];
 
     return (
-        <div>
+        <div className="wrapper">
+            <h2 className="sec-title">Blog Posts</h2>
             {posts.map(post => (
-                <div key={post.id}>
+                <div key={post.id} className="card">
                     <h2>{post.title}</h2>
                     <p>{post.content}</p>
                 </div>
