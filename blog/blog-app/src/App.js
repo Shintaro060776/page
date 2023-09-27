21c5a14f4c694aa74974219f165b64f89aa2ae85
shin20090317 <shintaro060776@gmail.com>
2023-09-27 22:39:15 +0900

    81

--- blog/blog-app/src/App.js
+++ blog/blog-app/src/App.js
@@ -6,12 +6,20 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename="/blog">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/post/:id" element={<PostDetail />} />
-      </Routes>
-    </Router>
+    <div>
+      <div className="video-background">
+        <video playsInline="playsinline" autoPlay="autoplay" muted="muted" loop="loop">
+          <source src="/background-video.mp4" type="video/mp4" />
+        </video>
+      </div>
+
+      <Router basename="/blog">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/post/:id" element={<PostDetail />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
