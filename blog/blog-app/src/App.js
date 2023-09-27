1dd0532df68d3c879c27ae78aae68428c133c5bb
shin20090317 <shintaro060776@gmail.com>
2023-09-27 18:25:50 +0900

    54

--- blog/blog-app/src/App.js
+++ blog/blog-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import PostDetail from './components/PostDetail';
 import AddPost from './components/AddPost';
@@ -7,11 +7,11 @@ import AddPost from './components/AddPost';
 function App() {
   return (
     <Router>
-      <Switch>
+      <Routes>
         <Route exact path="/blog" component={Home} />
         <Route path="/blog/post/:id" component={PostDetail} />
         <Route path="/blog/add" component={AddPost} />
-      </Switch>
+      </Routes>
     </Router>
   );
 }
