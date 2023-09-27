b6d36f37b30d1da0c029679342d39a4ce492b61e
shin20090317 <shintaro060776@gmail.com>
2023-09-27 21:47:14 +0900

    76

--- blog/blog-app/src/App.js
+++ blog/blog-app/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import PostDetail from './components/PostDetail';
-import AddPost from './components/AddPost';
 import './App.css';
 
 function App() {
