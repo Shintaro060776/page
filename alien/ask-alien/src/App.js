8c2b27d48810a0a9ce135fa3a1bbbe402ad2170d
shin20090317 <shintaro060776@gmail.com>
2023-10-22 23:50:55 +0900

    252

--- alien/ask-alien/src/App.js
+++ alien/ask-alien/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const { userInput, setUserInput } = useState('');
+  const [alienResponse, setAlienResponse] = useState('');
+
+  const handleInputChange = (event) => {
+    setUserInput(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    setAlienResponse('宇宙人の回答...')
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='video-container'>
+        <video src='video/138116_720p.mp4' autoPlay loop />
+      </div>
+      <div className='interaction-container'>
+        <input
+          type='text'
+          value={userInput}
+          onChange={handleInputChange}
+          placeholder='質問を入力'
+        />
+        <button onClick={handleSubmit}>Ask Alien</button>
+      </div>
+      <div className='response-container'>
+        <p>{alienResponse}</p>
+      </div>
     </div>
   );
 }
