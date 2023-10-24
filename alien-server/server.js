687fe1c9afca1fbcd46cd97a199c6fe4e546de8b
shin20090317 <shintaro060776@gmail.com>
2023-10-25 05:07:16 +0900

    313

--- alien-server/server.js
+++ alien-server/server.js
@@ -13,7 +13,16 @@ app.post('/ask-alien', async (req, res) => {
             body: JSON.stringify(req.body)
         };
         const response = await axios.post('https://r67d0rhvj9.execute-api.ap-northeast-1.amazonaws.com/prd/ask-alien', lambdaRequestBody);
-        res.json(response.data);
+
+        if (response.data && response.data.answer) {
+            res.json({ answer: response.data.answer });
+        } else if (response.data && response.data.error) {
+            console.error("Error from Lambda:", response.data.error);
+            res.status(400).json({ error: 'Error from Lambda function' });
+        } else {
+            console.error("Unexpected response from Lambda:", response.data);
+            res.status(500).json({ error: 'Internal server error' });
+        }
     } catch (error) {
         console.error("Error when calling Lambda:", error.message);
         res.status(500).json({ error: 'Internal server error' });
