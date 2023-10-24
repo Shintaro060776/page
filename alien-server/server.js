712740c64f40690dc014f2f4c26c247893a82996
shin20090317 <shintaro060776@gmail.com>
2023-10-25 04:41:54 +0900

    311

--- alien-server/server.js
+++ alien-server/server.js
@@ -7,12 +7,12 @@ const PORT = 3001;
 app.use(express.json());
 app.use(cors());
 
-app.post('/askalien', async (req, res) => {
+app.post('/ask-alien', async (req, res) => {
     try {
         const lambdaRequestBody = {
             body: JSON.stringify(req.body)
         };
-        const response = await axios.post('https://d7sh2cu0li.execute-api.ap-northeast-1.amazonaws.com/prd/askalien', lambdaRequestBody);
+        const response = await axios.post('https://r67d0rhvj9.execute-api.ap-northeast-1.amazonaws.com/prd/ask-alien', lambdaRequestBody);
         res.json(response.data);
     } catch (error) {
         console.error("Error when calling Lambda:", error.message);
